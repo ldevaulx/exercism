f06456c336b50565cd34ade8e7c5899b375dda8d
ldevaulx <ldevaulx@hotmail.com>
2015-04-20 15:24:21 +0200

    strtr

--- php/rna-transcription/rna-transcription.php
+++ php/rna-transcription/rna-transcription.php
@@ -1,19 +1,7 @@
 <?php
 
-function toRna($chaine)
+function toRna($str)
 {
-    $dna = ['G','C','T','A'];
-    $rna = ['C','G','A','U'];
-    $ret = '';
-
-    for ($i=0; $i< strlen($chaine);$i++)  {
-        if (($key = array_search($chaine[$i], $dna)) === false) {
-            throw new Exception("Only GCTA letters in DNA strand", 1);   
-        }
-        $ret = $ret . $rna[$key];
-    }
-
-    return $ret;
-
+    return strtr($str, 'GCTA', 'CGAU');
 }
 
